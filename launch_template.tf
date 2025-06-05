294462d656165c3743b17ed4d82020f63ec12fa9
Jacob Fiola <jacob.fiola@corelight.com>
2025-06-05 14:34:54 -0600

    test vuln fix behavior

--- launch_template.tf
+++ launch_template.tf
@@ -20,6 +20,10 @@ resource "aws_launch_template" "sensor_launch_template" {
     delete_on_termination = true
   }
 
+  metadata_options {
+    http_tokens = "required"
+  }
+
   user_data = module.sensor_config.cloudinit_config.rendered
 
   tags = var.tags
